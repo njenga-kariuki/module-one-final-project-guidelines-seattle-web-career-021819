6140bea2473a8b73a4b4225ef92bbe5ee88d4d57
github username <github email address>
2019-03-06 14:42:23 -0800

    adhoc

--- bin/cli.rb
+++ bin/cli.rb
@@ -9,7 +9,7 @@ p "We make it easy to track your favorite players stats through the NBA season."
 User.user_authentication
 # sleep 1
 # player_query = User.full_user_input_and_search
-
+#
 # player_id = Player.search_player_api_return_id(player_query)
 #
 # sorted_player_data = PlayerStat.search_stats_by_player_id(player_id)
